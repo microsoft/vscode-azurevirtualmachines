8f5a81324391589555e3c0943c02b5aecd7df31b
nturinski <naturins@microsoft.com>
2020-02-24 18:24:36 -0800

    Move adding protectedSettings outside of catch block

--- src/commands/addSshKey.ts
+++ src/commands/addSshKey.ts
@@ -6,7 +6,7 @@
 import { ComputeManagementClient, ComputeManagementModels } from "azure-arm-compute";
 import * as fse from "fs-extra";
 import { ProgressLocation, Uri, window } from "vscode";
-import { createAzureClient, IActionContext } from "vscode-azureextensionui";
+import { createAzureClient, IActionContext, IParsedError, parseError } from "vscode-azureextensionui";
 import { ext } from "../extensionVariables";
 import { localize } from "../localize";
 import { VirtualMachineTreeItem } from "../tree/VirtualMachineTreeItem";
@@ -34,13 +34,19 @@ export async function addSshKey(context: IActionContext, node?: VirtualMachineTr
         // the VMAccessForLinux extension is necessary to configure more SSH keys
         vmExtension = await computeClient.virtualMachineExtensions.get(node.resourceGroup, node.name, extensionName);
     } catch (e) {
-        vmExtension = { location: vm.location, publisher: 'Microsoft.OSTCExtensions', virtualMachineExtensionType: 'VMAccessForLinux', type: 'Microsoft.Compute/virtualMachines/extensions', typeHandlerVersion: '1.4', autoUpgradeMinorVersion: true };
-        vmExtension.protectedSettings = {
-            ssh_key: (await fse.readFile(sshPublicKey.fsPath)).toString(),
-            // tslint:disable-next-line: strict-boolean-expressions
-            username: vm.osProfile && vm.osProfile.adminUsername || 'azureuser'
-        };
+        const parsedError: IParsedError = parseError(e);
+        if (parsedError.errorType === 'ResourceNotFound') {
+            vmExtension = { location: vm.location, publisher: 'Microsoft.OSTCExtensions', virtualMachineExtensionType: 'VMAccessForLinux', type: 'Microsoft.Compute/virtualMachines/extensions', typeHandlerVersion: '1.4', autoUpgradeMinorVersion: true };
+        }
+
+        throw e;
     }
+
+    vmExtension.protectedSettings = {
+        ssh_key: (await fse.readFile(sshPublicKey.fsPath)).toString(),
+        // tslint:disable-next-line: strict-boolean-expressions
+        username: vm.osProfile && vm.osProfile.adminUsername || 'azureuser'
+    };
 
     const configuringSsh: string = localize('ConfiguringSsh', 'Configuring virtual machine "{0}" with SSH Public Key "{1}"...', node.name, sshPublicKey.fsPath);
     const configuringSshSucceeded: string = localize('ConfiguringSshSucceeded', 'Successfully configured "{0}".', node.name);
