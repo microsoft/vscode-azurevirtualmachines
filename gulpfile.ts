303fde0f4d00c7ae5c93cb2797c95d7acff33d59
Eric Jizba <erijiz@microsoft.com>
2020-04-01 14:53:32 -0700

    Engineering work to make our repos more consistent 👷🏻‍ (#96)

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,18 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// tslint:disable:no-console
 // tslint:disable:no-implicit-dependencies (this allows the use of dev dependencies)
 
-// Grandfathered in
-// tslint:disable:typedef
-// tslint:disable:no-unsafe-any
-
-import * as cp from 'child_process';
 import * as fse from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
 import { gulp_installAzureAccount, gulp_webpack } from 'vscode-azureextensiondev';
+
+declare let exports: { [key: string]: unknown };
 
 async function prepareForWebpack(): Promise<void> {
     const mainJsPath: string = path.join(__dirname, 'main.js');
@@ -25,15 +21,6 @@ async function prepareForWebpack(): Promise<void> {
     await fse.writeFile(mainJsPath, contents);
 }
 
-function test() {
-    const env = process.env;
-    env.DEBUGTELEMETRY = '1';
-    env.MOCHA_timeout = String(20 * 1000);
-    env.CODE_TESTS_WORKSPACE = path.join(__dirname, 'test/test.code-workspace');
-    env.CODE_TESTS_PATH = path.join(__dirname, 'dist/test');
-    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-}
-
 exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
 exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
-exports.test = gulp.series(gulp_installAzureAccount, test);
+exports.preTest = gulp_installAzureAccount;
