249331a0522b8a903e5f35e730df779067187b3b
nturinski <naturins@microsoft.com>
2020-08-24 14:30:52 -0700

    Add restart

---
+++ src/commands/restartVirtualMachine.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { ComputeManagementClient } from "@azure/arm-compute";
+import { createAzureClient, IActionContext } from "vscode-azureextensionui";
+import { ext } from "../extensionVariables";
+import { localize } from "../localize";
+import { VirtualMachineTreeItem } from "../tree/VirtualMachineTreeItem";
+import { nonNullValue } from "../utils/nonNull";
+
+export async function restartVirtualMachine(context: IActionContext, node?: VirtualMachineTreeItem): Promise<void> {
+    if (!node) {
+        node = await ext.tree.showTreeItemPicker<VirtualMachineTreeItem>(VirtualMachineTreeItem.contextValue, context);
+    }
+
+    const computeClient: ComputeManagementClient = createAzureClient(node.root, ComputeManagementClient);
+
+    await node.runWithTemporaryDescription(localize('restarting', 'Restarting...'), async () => {
+        const vmti: VirtualMachineTreeItem = nonNullValue(node);
+        ext.outputChannel.appendLog(localize('restartingVm', `Restarting "${vmti.name}"...`));
+        await computeClient.virtualMachines.restart(vmti.resourceGroup, vmti.name);
+        ext.outputChannel.appendLog(localize('restartedVm', `"${vmti.name}" has been restarted.`));
+    });
+
+}
