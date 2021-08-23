205ab558f4448b1656d4822d7b51b1ed5b0a1b12
alexweininger <alex.weininger@live.com>
2021-08-23 15:53:38 -0500

    update locations

--- test/nightly/getRotatingValue.ts
+++ test/nightly/getRotatingValue.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const locations = [];
+import { locations } from "./global.resource.test";
 
 let locationCount: number = getStartingIndex();
 export function getRotatingLocation(): string {
