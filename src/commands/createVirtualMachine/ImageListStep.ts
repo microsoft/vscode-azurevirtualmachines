9835bb1c55405636dd06fcc62d70808d41ddcb4d
nturinski <naturins@microsoft.com>
2021-09-03 14:52:27 -0700

    Clean up

--- src/commands/createVirtualMachine/ImageListStep.ts
+++ src/commands/createVirtualMachine/ImageListStep.ts
@@ -20,6 +20,7 @@ export class ImageListStep extends AzureWizardPromptStep<IVirtualMachineWizardCo
         const image = (await context.ui.showQuickPick(picks, { placeHolder }));
         context.telemetry.properties.image = image.label;
 
+        // if there was no data, it is not a featured image and we have a constant map for these
         if (image.data === undefined) {
             context.image = extraImagesMap[image.label];
         } else {
@@ -127,7 +128,6 @@ type OfferFromLegacyPlanId = {
     plans: PlanFromLegacyPlanId[]
 };
 
-<<<<<<< HEAD
 type PlanFromLegacyPlanId = {
     id: string,
     artifacts: PlanArtifacts[]
@@ -147,88 +147,3 @@ type UiDefinition = {
 
     }
 };
-
-=======
-export const linuxImages: ImageReferenceWithLabel[] = [
-    ubuntu1804LTSImage,
-    {
-        label: 'Red Hat Enterprise Linux 8.2 (LVM) - Gen1',
-        publisher: 'RedHat',
-        offer: 'RHEL',
-        sku: '8.2',
-        version: 'latest'
-    },
-    {
-        label: 'SUSE Enterprise Linux 15 SP2 - Gen1',
-        publisher: 'suse',
-        offer: 'sles-15-sp2-basic',
-        sku: 'gen1',
-        version: 'latest'
-    },
-    {
-        label: 'CentOS-based 8.2 - Gen1',
-        publisher: 'OpenLogic',
-        offer: 'CentOS',
-        sku: '8_2',
-        version: 'latest'
-    },
-    {
-        label: 'Debian 10 "Buster" - Gen1',
-        publisher: 'debian',
-        offer: 'debian-10',
-        sku: '10',
-        version: 'latest'
-    },
-    {
-        label: 'Oracle Linux 7.8 - Gen1',
-        publisher: 'Oracle',
-        offer: 'Oracle-Linux',
-        sku: '78',
-        version: 'latest'
-    },
-    {
-        label: 'Ubuntu Server 16.04 LTS - Gen1',
-        publisher: 'Canonical',
-        offer: 'UbuntuServer',
-        sku: '16.04-LTS',
-        version: 'latest'
-    },
-    {
-        label: 'Data Science Virtual Machine - Ubuntu 18.04 - Gen1',
-        publisher: 'microsoft-dsvm',
-        offer: 'ubuntu-1804',
-        sku: '1804',
-        version: 'latest'
-    }
-];
-
-export const windowsImages: ImageReferenceWithLabel[] = [
-    {
-        label: 'Windows Server 2019 Datacenter - Gen 1',
-        publisher: 'MicrosoftWindowsServer',
-        offer: 'WindowsServer',
-        sku: '2019-Datacenter',
-        version: 'latest'
-    },
-    {
-        label: 'Windows Server 2016 Datacenter - Gen 1',
-        publisher: 'MicrosoftWindowsServer',
-        offer: 'WindowsServer',
-        sku: '2016-Datacenter',
-        version: 'latest'
-    },
-    {
-        label: 'Windows Server 2012 R2 Datacenter - Gen 1',
-        publisher: 'MicrosoftWindowsServer',
-        offer: 'WindowsServer',
-        sku: '2012-R2-Datacenter',
-        version: 'latest'
-    },
-    {
-        label: 'Windows 10 Pro, Version 20H2 - Gen 1',
-        publisher: 'MicrosoftWindowsDesktop',
-        offer: 'Windows-10',
-        sku: '20h2-pro',
-        version: 'latest'
-    }];
->>>>>>> 8a7c3d03a2c398879ea2775ac6ec4eb6b5d1a1e8
