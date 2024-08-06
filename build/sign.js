89d9853781a6ad97e0b8d7268806a83104c877e6
alexweininger <alex.weininger@live.com>
2024-08-06 10:54:59 -0500

    sign the signature file not the vsix

--- build/sign.js
+++ build/sign.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { signFiles } from './SignFiles.js';
 
 const result = signFiles({
-    '4014052': [path.join(process.env.BUILD_SOURCESDIRECTORY, 'extension.vsix')],
+    '4014052': [path.join(process.env.BUILD_SOURCESDIRECTORY, 'extension.signature.p7s')],
 });
 
 process.exitCode = result;
