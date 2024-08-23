4255ccb533615e54e821b0710c0e6fb6c257761a
alexweininger <alex.weininger@live.com>
2024-08-23 14:25:20 -0400

    Fixup

--- build/sign.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { signFiles } from './SignFiles.js';
-
-const result = signFiles({
-    '4014052': [path.join(process.env.BUILD_SOURCESDIRECTORY, 'extension.signature.p7s')],
-});
-
-process.exitCode = result;
