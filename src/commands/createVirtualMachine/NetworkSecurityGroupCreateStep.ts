ef256e1485ccbe32d253faf997aad843bf7d6f6f
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2025-05-13 10:39:41 -0700

    Add activity children for `createVirtualMachine` (#449)

--- src/commands/createVirtualMachine/NetworkSecurityGroupCreateStep.ts
+++ src/commands/createVirtualMachine/NetworkSecurityGroupCreateStep.ts
@@ -5,23 +5,27 @@
 
 import { type NetworkManagementClient, type NetworkSecurityGroup, type SecurityRule } from '@azure/arm-network';
 import { LocationListStep } from "@microsoft/vscode-azext-azureutils";
-import { AzureWizardExecuteStep, nonNullProp, nonNullValueAndProp } from "@microsoft/vscode-azext-utils";
+import { AzureWizardExecuteStepWithActivityOutput, nonNullProp, nonNullValueAndProp } from "@microsoft/vscode-azext-utils";
 import { type Progress } from "vscode";
-import { ext } from '../../extensionVariables';
 import { localize } from '../../localize';
 import { createNetworkClient } from '../../utils/azureClients';
 import { type IVirtualMachineWizardContext } from './IVirtualMachineWizardContext';
 
-export class NetworkSecurityGroupCreateStep extends AzureWizardExecuteStep<IVirtualMachineWizardContext> {
+export class NetworkSecurityGroupCreateStep<T extends IVirtualMachineWizardContext> extends AzureWizardExecuteStepWithActivityOutput<T> {
     public priority: number = 220;
+    public stepName: string = 'networkSecurityGroupCreateStep';
+    protected getOutputLogSuccess = (context: T) => localize('createNetworkSecurityGroupSuccess', 'Successfully created network security group "{0}" with inbound ports "{1}" enabled.', generateNsgName(nonNullProp(context, 'newVirtualMachineName')), this.enabledPorts.join(', '));
+    protected getOutputLogFail = (context: T) => localize('createNetworkSecurityGroupFail', 'Failed to create network security group "{0}".', generateNsgName(nonNullProp(context, 'newVirtualMachineName')));
+    protected getTreeItemLabel = (context: T) => localize('createNetworkSecurityGroupLabel', 'Create network security group "{0}"', generateNsgName(nonNullProp(context, 'newVirtualMachineName')));
 
-    public async execute(context: IVirtualMachineWizardContext, progress: Progress<{ message?: string | undefined; increment?: number | undefined }>): Promise<void> {
+    private enabledPorts: string[] = [];
+
+    public async execute(context: T, progress: Progress<{ message?: string | undefined; increment?: number | undefined }>): Promise<void> {
+        progress.report({ message: localize('creatingNsg', 'Creating network security group...') });
+
         const networkClient: NetworkManagementClient = await createNetworkClient(context);
         const location: string = (await LocationListStep.getLocation(context)).name
-
-        // when creating a VM on the portal, this is the suffix that is added to the network security group
-        const nsgName: string = nonNullProp(context, 'newVirtualMachineName') + '-nsg';
-
+        const nsgName: string = generateNsgName(nonNullProp(context, 'newVirtualMachineName'));
         const securityRules: SecurityRule[] = context.os !== 'Windows' ? [
             { name: 'SSH', protocol: 'Tcp', sourcePortRange: '*', destinationPortRange: '22', sourceAddressPrefix: '*', destinationAddressPrefix: '*', access: 'Allow', priority: 340, direction: 'Inbound' },
             { name: 'HTTPS', protocol: 'Tcp', sourcePortRange: '*', destinationPortRange: '443', sourceAddressPrefix: '*', destinationAddressPrefix: '*', access: 'Allow', priority: 320, direction: 'Inbound' },
@@ -33,20 +37,20 @@ export class NetworkSecurityGroupCreateStep extends AzureWizardExecuteStep<IVirt
             name: nsgName, location, securityRules
         };
 
-        const enabledPorts: string[] = nonNullProp(networkSecurityGroupProps, 'securityRules').map(rule => nonNullProp(rule, 'destinationPortRange'));
+        this.enabledPorts = nonNullProp(networkSecurityGroupProps, 'securityRules').map(rule => nonNullProp(rule, 'destinationPortRange'));
 
-        const creatingNsg: string = localize('creatingNsg', `Creating new network security group "${nsgName}" with inbound ports ${enabledPorts.join(', ')} enabled...`);
         const rgName: string = nonNullValueAndProp(context.resourceGroup, 'name');
-        ext.outputChannel.appendLog(creatingNsg);
-        progress.report({ message: creatingNsg });
-
         await networkClient.networkSecurityGroups.beginCreateOrUpdateAndWait(rgName, nsgName, networkSecurityGroupProps);
         // workaround for https://github.com/Azure/azure-sdk-for-js/issues/20249
         context.networkSecurityGroup = await networkClient.networkSecurityGroups.get(rgName, nsgName);
-        ext.outputChannel.appendLog(localize('createdNsg', `Created new network security group "${nsgName}".`));
     }
 
-    public shouldExecute(context: IVirtualMachineWizardContext): boolean {
+    public shouldExecute(context: T): boolean {
         return !context.networkSecurityGroup;
     }
+}
+
+// when creating a VM on the portal, this is the suffix that is added to the network security group
+function generateNsgName(newVirtualMachineName: string): string {
+    return newVirtualMachineName + '-nsg';
 }
